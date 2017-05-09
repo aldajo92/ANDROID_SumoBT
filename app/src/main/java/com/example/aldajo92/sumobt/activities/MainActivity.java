6ec093eb542407b618ec31e78c57fbc62699e08e
Alejandro Gomez <alejandro.gomez@rappi.com>
2017-05-09 18:15:56 -0500

    adding controller

--- app/src/main/java/com/example/aldajo92/sumobt/activities/MainActivity.java
+++ app/src/main/java/com/example/aldajo92/sumobt/activities/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.aldajo92.sumobt.activities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.InputDevice;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 
 import com.example.aldajo92.sumobt.R;
 import com.jmedeisis.bugstick.Joystick;
@@ -29,22 +33,142 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onDrag(float degrees, float offset) {
                 showlog("drag degree=" + degrees + " offset=" + offset);
-                showlog("drag v=" +
+                showlog("m;" +
                         (int) (offset * 400 * Math.sin(Math.toRadians(degrees))) +
-                        " w=" +
+                        ";" +
                         (int) (offset * 400 * Math.cos(Math.toRadians(degrees)))
                 );
                 if (getBluetoothService().getIsBTConnected()) {
-                    sendMessage("m-" + (int) offset * 400 + "-" );
+                    sendMessage("m;" +
+                            (int) (offset * 400 * Math.sin(Math.toRadians(degrees))) +
+                            ";" +
+                            (int) (offset * 400 * Math.cos(Math.toRadians(degrees)))
+                    );
                 }
             }
 
             @Override
             public void onUp() {
-                sendMessage("f-0-0");
+                sendMessage("f;0;0");
             }
         });
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        boolean handled = false;
+        if ((event.getSource() & InputDevice.SOURCE_GAMEPAD)
+                == InputDevice.SOURCE_GAMEPAD) {
+            if (event.getRepeatCount() == 0) {
+                showlog("onKeyDown: " + keyCode);
+//                switch (keyCode) {
+//                    // Handle gamepad and D-pad button presses to
+//                    // navigate the ship
+//
+//                    default:
+//                        if (isFireKey(keyCode)) {
+//                            // Update the ship object to fire lasers
+//                            handled = true;
+//                        }
+//                        break;
+//                }
+            }
+            if (handled) {
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        // Check that the event came from a game controller
+        if ((event.getSource() & InputDevice.SOURCE_JOYSTICK) ==
+                InputDevice.SOURCE_JOYSTICK &&
+                event.getAction() == MotionEvent.ACTION_MOVE) {
+
+            // Process all historical movement samples in the batch
+            final int historySize = event.getHistorySize();
+
+            // Process the movements starting from the
+            // earliest historical position in the batch
+            for (int i = 0; i < historySize; i++) {
+                // Process the event at historical position i
+                processJoystickInput(event, i);
+            }
+
+            // Process the current movement sample in the batch (position -1)
+            processJoystickInput(event, -1);
+            return true;
+        }
+
+        return super.onGenericMotionEvent(event);
+    }
+
+    private void processJoystickInput(MotionEvent event, int historyPos) {
+
+        InputDevice mInputDevice = event.getDevice();
+
+        // Calculate the horizontal distance to move by
+        // using the input value from one of these physical controls:
+        // the left control stick, hat axis, or the right control stick.
+        float x = getCenteredAxis(event, mInputDevice,
+                MotionEvent.AXIS_X, historyPos);
+        if (x == 0) {
+            x = getCenteredAxis(event, mInputDevice,
+                    MotionEvent.AXIS_HAT_X, historyPos);
+        }
+        if (x == 0) {
+            x = getCenteredAxis(event, mInputDevice,
+                    MotionEvent.AXIS_Z, historyPos);
+        }
+
+        // Calculate the vertical distance to move by
+        // using the input value from one of these physical controls:
+        // the left control stick, hat switch, or the right control stick.
+        float y = getCenteredAxis(event, mInputDevice,
+                MotionEvent.AXIS_Y, historyPos);
+        if (y == 0) {
+            y = getCenteredAxis(event, mInputDevice,
+                    MotionEvent.AXIS_HAT_Y, historyPos);
+        }
+        if (y == 0) {
+            y = getCenteredAxis(event, mInputDevice,
+                    MotionEvent.AXIS_RZ, historyPos);
+        }
+
+        // Update the ship object based on the new x and y values
+        Log.i("CONTROL", "processJoystickInput x: " + x);
+        Log.i("CONTROL", "processJoystickInput y: " + y);
+
+        if (getBluetoothService().getIsBTConnected()) {
+            sendMessage("m;" +
+                    (int) (-y * 200) +
+                    ";" +
+                    (int) (x * 200)
+            );
+        }
+    }
+
+    private static float getCenteredAxis(MotionEvent event, InputDevice device, int axis, int historyPos) {
+        final InputDevice.MotionRange range =
+                device.getMotionRange(axis, event.getSource());
+
+        // A joystick at rest does not always report an absolute position of
+        // (0,0). Use the getFlat() method to determine the range of values
+        // bounding the joystick axis center.
+        if (range != null) {
+            final float flat = range.getFlat();
+            final float value =
+                    historyPos < 0 ? event.getAxisValue(axis) :
+                            event.getHistoricalAxisValue(axis, historyPos);
+
+            // Ignore axis values that are within the 'flat' region of the
+            // joystick axis center.
+            if (Math.abs(value) > flat) {
+                return value;
+            }
+        }
+        return 0;
+    }
 }
